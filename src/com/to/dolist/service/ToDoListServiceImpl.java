87d505d5eaf5f4082bc39b90a3375c9d3aeab09d
Arunkumar Angappan <arunkumar.angappan@object-frontier.com>
2019-04-05 15:58:11 +0530

    Validation added into the service

--- src/com/to/dolist/service/ToDoListServiceImpl.java
+++ src/com/to/dolist/service/ToDoListServiceImpl.java
@@ -1,5 +1,6 @@
 package com.to.dolist.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import com.to.dolist.dao.ToDoListDAO;
 import com.to.dolist.model.ToDoList;
+import com.to.dolist.utilities.AppException;
+import com.to.dolist.utilities.ErrorCodes;
 
 @Repository
 @Service
@@ -15,14 +18,21 @@ public class ToDoListServiceImpl implements ToDoListService{
 
 	@Autowired
 	ToDoListDAO toDao;
+
 	@Override
 	public ToDoList create(ToDoList list) {
+		validate(list);
 		return toDao.create(list);
 	}
 
 	@Override
 	public ToDoList update(ToDoList list) {
-		return toDao.update(list);
+		validate(list);
+		if (list.getId()!= 0) {			
+			return toDao.update(list);
+		} else {
+			throw new AppException(ErrorCodes.ID_ERROR);
+		}
 	}
 
 	@Override
@@ -40,4 +50,29 @@ public class ToDoListServiceImpl implements ToDoListService{
 		toDao.delete(id);
 	}
 
+	private void validate(ToDoList list) {
+
+		List<ErrorCodes> execption = new ArrayList<>();
+		if ("".equals(list.getTitle().trim())) {
+			execption.add(ErrorCodes.TITLE_FILED_EMPTY);
+		}
+		if ("".equals(list.getMessgae().trim())) {
+			execption.add(ErrorCodes.MESSAGE_ERROR);
+		}
+		if ("".equals(list.getStatus().trim())) {
+			execption.add(ErrorCodes.STATUS_ERROR);
+		}
+		if ("".equals(list.getStartdate().toString().trim())) {
+			execption.add(ErrorCodes.START_DATE_ERROR);
+		}
+		if ("".equals(list.getDuedate().toString().trim())) {
+			execption.add(ErrorCodes.DUE_DATE_ERROR);
+		}
+		if (list.getEstimation() == 0) {
+			execption.add(ErrorCodes.ESTIMATION_ERROR);
+		}
+		if (!execption.isEmpty()) {
+			throw new AppException(execption);
+		}
+	}
 }
